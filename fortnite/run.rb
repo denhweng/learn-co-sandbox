4cb15b13d95ae5931256c1fd22adb1c7c85e6f70
Dan Hwang <danhwangfkpc@gmail.com>
2019-07-06 04:06:54 +0000

    Automatically backed up by Learn

--- fortnite/run.rb
+++
@@ -1,9 +0,0 @@
-require "pry"
-require_relative "character"
-require_relative "game"
-
-first_level = Game.new("Forest", "Beginner")
-main_char = Character.new("Dan", "Super Ruby Powers")
-main_char.game = first_level
-first_level.add_character(main_char)
-binding.pry
